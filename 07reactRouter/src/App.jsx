3a777a77710f11637b9f5c432b613c689dc01651
g-nesh <ganeshnimbare25@gmail.com>
2024-01-21 02:16:09 +0530

    creating some components

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='bg-green-600 p-4'>React Router</h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
